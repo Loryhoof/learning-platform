77056e6b02a4711ad3274630d8204ea672103822
Kevin <klattkev@gmail.com>
2024-04-21 02:41:33 +0200

    absolute dogwater code

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
 "use client"
 
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { thaiVocabulary } from "./categories/thai-100";
+import { shuffleArray } from "./utils";
 
 function RevealCard({lesson, onCloseReveal}: any) {
 
@@ -62,6 +63,27 @@ function Card({lesson, onNextLesson}: any) {
     )
   }
 
+  function InputCard() {
+    return (
+      <div className="flex flex-col items-center text-black text-lg font-semibold p-2">
+        <textarea ref={textAreaRef} onChange={handleInput} placeholder="Type the English word..." className="bg-black text-white p-2 resize-none w-full mt-2 border"></textarea>
+        <div className="flex flex-row gap-4 w-full">
+          <button onClick={handleReveal} className="bg-white rounded-lg p-2 w-full mt-5">Peek 👀</button>
+          <button onClick={handleCheck} className="bg-white rounded-lg p-2 w-full mt-5">Submit</button>
+        </div>
+        <div className={`${statusText === "Correct" ? `text-green-500`: `text-red-500`} mt-4`}>{statusText}</div>
+      </div>
+    )
+  }
+
+  function PickCard() {
+    return (
+      <div className="flex flex-col items-center text-black text-lg font-semibold p-2">
+
+      </div>
+    )
+  }
+
   const handleCheck = () => {
 
     setTimeout(() => {
@@ -109,6 +131,12 @@ function Card({lesson, onNextLesson}: any) {
     }
   }
 
+  const handleRenderInputMethod = () => {
+    return (
+      <InputCard/>
+    )
+  }
+
   return (
     <div className="bg-black w-1/2 rounded-lg">
       {isReveal ? (
@@ -117,21 +145,66 @@ function Card({lesson, onNextLesson}: any) {
         </>
       ) :
       (
-      <div className="flex flex-col items-center text-black text-lg font-semibold p-2">
-        <textarea ref={textAreaRef} onChange={handleInput} placeholder="Type the English word..." className="bg-black text-white p-2 resize-none w-full mt-2 border"></textarea>
-        <div className="flex flex-row gap-4 w-full">
-          <button onClick={handleReveal} className="bg-white rounded-lg p-2 w-full mt-5">Peek 👀</button>
-          <button onClick={handleCheck} className="bg-white rounded-lg p-2 w-full mt-5">Submit</button>
-        </div>
-        <div className={`${statusText === "Correct" ? `text-green-500`: `text-red-500`} mt-4`}>{statusText}</div>
-      </div>
-      )}
+        handleRenderInputMethod()
+      )
+      }
     </div>
   )
 }
 
 const lessons = thaiVocabulary
 const course = "Thai"
+
+const ChoiceElement = ({str, onClick}: any) => {
+  return (
+    <button onClick={onClick} className="p-4 border rounded-lg items-center text-center hover:bg-slate-800">{str}</button>
+  )
+}
+
+function PickChoice({lessons, currentIndex, handleChoice}: any) {
+
+  const [hasMounted, setHasMounted] = useState(false); // <-- add this
+
+  useEffect(() => {
+    setHasMounted(true); // <-- toggle on client-side, because useEffect doesn't run on server-side/during SSG build
+  }, []);
+
+  let randomizedArray = [] as any
+
+  if(Array.isArray(lessons[currentIndex].answer)) {
+    randomizedArray[0] = lessons[currentIndex].answer[0]
+  }
+  else {
+    randomizedArray[0] = lessons[currentIndex].answer
+  }
+
+  while(randomizedArray.length < 4) {
+    let randomElement = lessons[Math.floor(Math.random() * lessons.length)].answer;
+
+    if(Array.isArray(randomElement)) {
+      randomElement = randomElement[0]
+    }
+
+    if(randomElement != lessons[currentIndex].answer) {
+      randomizedArray.push(randomElement)
+    }
+  }
+
+  shuffleArray(randomizedArray)
+
+  return (
+    <>
+    {hasMounted && (
+    <div className="flex flex-row gap-4">
+      <ChoiceElement onClick= {() => handleChoice(randomizedArray[0])} str={randomizedArray[0]}></ChoiceElement>
+      <ChoiceElement onClick= {() => handleChoice(randomizedArray[1])} str={randomizedArray[1]}></ChoiceElement>
+      <ChoiceElement onClick= {() => handleChoice(randomizedArray[2])} str={randomizedArray[2]}></ChoiceElement>
+      <ChoiceElement onClick= {() => handleChoice(randomizedArray[3])} str={randomizedArray[3]}></ChoiceElement>
+    </div>
+    )}
+    </>
+  )
+}
 
 export default function Home() {
 
@@ -148,6 +221,38 @@ export default function Home() {
 
   const handleRomanizedChange = () => {
     setShowRomanized(!showRomanized)
+  }
+
+  const handleChoice = (str: string) => {
+    if(str.length == 0) {
+      return
+    }
+
+    let stringToCheckWith = lessons[lessonIndex].answer
+
+    if(Array.isArray(lessons[lessonIndex].answer)) {
+      stringToCheckWith = lessons[lessonIndex].answer[0]
+    }
+
+    if(str.trim().toLowerCase() == stringToCheckWith.toString().toLowerCase()) {
+      console.log("correct!!!!!!!!")
+      onNextLesson()
+    }
+    else {
+      console.log("wrong")
+    }
+  }
+
+  const handleRenderLesson = () => {
+
+    if(Math.random() > 0.5) {
+      return (
+        <PickChoice lessons={lessons} currentIndex={lessonIndex} handleChoice={handleChoice}></PickChoice>
+      )
+    }
+    return (
+      <Card lesson={lessons[lessonIndex]} onNextLesson={onNextLesson}></Card>
+    )
   }
 
   return (
@@ -169,7 +274,7 @@ export default function Home() {
           <div className="mb-4 font-semibold text-4xl text-yellow-400">
             {lessons[lessonIndex].question} {showRomanized ? `- ${lessons[lessonIndex].romanized}` : ''}
           </div>
-          <Card lesson={lessons[lessonIndex]} onNextLesson={onNextLesson}></Card>
+          {handleRenderLesson()}
         </div>
       </>
       } 
